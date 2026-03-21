75d9836fe4da7570a104c37ecf1717fe6ab10ae8
Abhishek Kumar <abhishekkumar591508@gmail.com>
2026-03-21 13:21:02 +0530

    Add feature of signup and getAllUser

--- Controllers/Auth.js
+++ Controllers/Auth.js
@@ -1,11 +1,46 @@
-const userSchema = require('../models/User')
+const user = require('../models/user')  // import model to communicate with DB
+const bcrypt = require('bcrypt');   //import bcrypt for hashing password
 
-const login = async (req, res)=>{
-
-}
 
 const signup = async (req,res)=>{
+    try{
+        const{name, email, password, role} = req.body;
+        const isUser = await user.findOne({email});
+        if(isUser){
+            return res.status(400).json({
+                success: false,
+                message:"User Already exist"
+            })
+        }
+        let hashedPassword;
+        try{
+            hashedPassword = await bcrypt.hash(password, 10);//mistake-bcrypt is asynchronous and hence should be awaited
+        }catch(error){
+            return res.status(500).json({
+                success: false,
+                message: "Error in hashing password"
+            })
+        }
+         
+        const createData = await user.create({
+            name, email, password:hashedPassword, role
+        })
+        return res.status(200).json({   //res.status not createData.status 
+            success:true,
+            data:createData,
+            message:"Data created Successfully"
+        })
+    }
+    catch(error){
+        console.log("Error in signup", error);
+        return res.status(500).json({
+            success: false,
+            message: "Error in Signup",
+            error: error.message
+        })
 
+    }
 }
 
-module.exports = {login, signup};
+
+module.exports = {signup};
--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 //contorller
-const {login, signup} = require('../Controllers/Auth');
+const {signup} = require('../controllers/Auth');
+const {getAllUsers} = require('../controllers/getuser');
 
 //map path to controller
-router.post('/login', login);
+// router.post('/login', login);
 router.post('/signup', signup);
+router.get('/getUsers', getAllUsers);
 
 module.exports = router;
